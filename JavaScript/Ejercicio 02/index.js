e2d8ecdaf24b83761588170f6821f23b056aeffc
Ap4rici0 <111245927+Ap4rici0@users.noreply.github.com>
2023-04-09 19:39:15 +0200

    Create index.js

---
+++ JavaScript/Ejercicio 02/index.js
@@ -0,0 +1,11 @@
+let fecha_nacimiento = new Date(1989, 7, 04);
+let libro = {
+    título: "La voz de las espadas",
+    autor: "Joe Abercrombie",
+    fecha: new Date(2005, 2, 28),
+    url: "www.amazon.com"
+}
+
+let ejercicio = ["Ramon", 34, false, fecha_nacimiento, libro];
+
+console.log(ejercicio);
