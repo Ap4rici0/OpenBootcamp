b99ff7799ae5cee39d2702808b0c916aaae7d18e
Ap4rici0 <111245927+Ap4rici0@users.noreply.github.com>
2023-04-14 13:33:45 +0200

    Update index.js

--- JavaScript/Ejercicio 05/index.js
+++ JavaScript/Ejercicio 05/index.js
@@ -1,10 +1,8 @@
 let altura_cm = 175;
-let apellido_m = 1.75;
+let altura_m = 1.75;
 let peso_kg = 75.50;
 
-let redondeado = apellido_m.toFixed(0);
-console.log(redondeado);
+const altura_redondeada = Math.ceil(altura_m)
+const peso_redondeado = Math.floor(peso_kg)
 
-let peso_redondeado = peso_kg.toFixed(0) - 1;
-console.log(peso_redondeado);
-
+const sonIguales = Number.MAX_VALUE + 1 === Number.MAX_VALUE
