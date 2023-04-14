715719443c1facec0edd26f16d0fa1547aaebaa4
Ap4rici0 <111245927+Ap4rici0@users.noreply.github.com>
2023-04-14 13:31:37 +0200

    Create index.js

---
+++ JavaScript/Ejercicio 05/index.js
@@ -0,0 +1,10 @@
+let altura_cm = 175;
+let apellido_m = 1.75;
+let peso_kg = 75.50;
+
+let redondeado = apellido_m.toFixed(0);
+console.log(redondeado);
+
+let peso_redondeado = peso_kg.toFixed(0) - 1;
+console.log(peso_redondeado);
+
