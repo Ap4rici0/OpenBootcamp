cb89666b0b99f09fec40e4b4ce14c63058a3b09c
Ap4rici0 <111245927+Ap4rici0@users.noreply.github.com>
2023-04-18 14:32:54 +0200

    Create index.js

---
+++ JavaScript/Ejercicio 07/index.js
@@ -0,0 +1,8 @@
+let familia = ["Ramón Fernandez", "Isabel Losada", "Alejandro Fernandez", "Ro y Marga Fernandez"];
+let nombresFamilia = new Set(familia);
+
+nombresFamilia.add("Ramón Fernandez");
+console.log(nombresFamilia);
+
+nombresFamilia.add("JavaScript");
+console.log(nombresFamilia);
