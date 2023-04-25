f428dbf21e969f9b2e8c71059fc8d889b5c27d72
Ap4rici0 <111245927+Ap4rici0@users.noreply.github.com>
2023-04-25 08:18:33 +0200

    Create index.js

---
+++ JavaScript/Ejercicio 11/index.js
@@ -0,0 +1,16 @@
+function verdadero(){
+    return true
+}
+
+async function promesa(){
+    return setTimeout(() => {
+       console.log("Hola soy una promesa") 
+    }, 5000);
+}
+
+function* pares(){
+    let id = 0
+    while(true){
+        yield id += 2
+    }
+}
